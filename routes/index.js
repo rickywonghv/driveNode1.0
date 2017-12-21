ae3cf64ce6a29a1d1a6da023bd168e98eebd5e59
rickywonghv <damonwonghv@gmail.com>
2017-12-21 22:59:55 +0800

    Initial redirect to init.jade

--- routes/index.js
+++ routes/index.js
@@ -1,43 +1,46 @@
 var express = require('express');
 var router = express.Router();
-var path=require("path");
-var Auth=require("../model/auth.js");
-var Init=require("../model/initial.js");
+var path = require("path");
+var Auth = require("../model/auth.js");
+var Init = require("../model/initial.js");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  ckTk(req,res,"index",'Drive');
+    ckTk(req, res, "index", 'Drive');
 });
 
 router.get('/view', function(req, res, next) {
-  ckTk(req,res,"view",'Drive');
+    ckTk(req, res, "view", 'Drive');
 });
 
-router.get('/login',function(req,res,next){
-  res.render("login",{title:"Drive"});
+router.get('/login', function(req, res, next) {
+    res.render("login", { title: "Drive" });
 });
 
-router.get('/init',function(req,res,next){
-  Init.Init(function(data){
-    res.json(data);
-  })
+router.get('/init', function(req, res, next) {
+    /*
+    Init.Init(function(data) {
+        res.json(data);
+    })
+    */
+    res.render("init", { title: "Drive" });
 });
 
-function ckTk(req,res,jade,title){
-  Auth.ckTk(req.cookies.token,function(data){
-    if(data.success){
-      res.render(jade,{title:title});
-    }else{
-      Init.CkInstall(function(data){
-        if(data){
-          res.render("login",{title:"Drive"});
-        }else{
-          res.redirect('/init');
+function ckTk(req, res, jade, title) {
+    Auth.ckTk(req.cookies.token, function(data) {
+        if (data.success) {
+            res.render(jade, { title: title });
+        } else {
+            Init.CkInstall(function(data) {
+                if (data) {
+                    res.render("login", { title: "Drive" });
+                } else {
+                    res.redirect('/init');
+                }
+            })
+
         }
-      })
-
-    }
-  });
+    });
 }
 
 module.exports = router;
