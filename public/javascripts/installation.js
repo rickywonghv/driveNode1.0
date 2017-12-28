e84da241e4a8c27e84cbdf7f1a6c22c8d23b0a56
Damon Wong <cw_w@outlook.com>
2017-12-28 20:37:39 +0800

    Installation JS

--- public/javascripts/installation.js
+++ public/javascripts/installation.js
@@ -1,10 +1,26 @@
 var app=angular.module('initApp', ['ngMaterial','ngCookies','angular-loading-bar','ngAnimate']);
 app.controller('initCtrl',function($scope,$window,$interval,cfpLoadingBar,initService){
+  $scope.backBtnSuccess=true;
+  $scope.backDrive=function(){
+    if(!$scope.backBtnSuccess){
+      
+    }
+  }
   $scope.initial=function(){
     var password=$scope.init.password;
     var conpassword=$scope.init.confirmPassword;
     initService.initial(password,conpassword,function(data){
-      
+      console.log(data);
+      if(data){
+        if(data.success){
+          console.log(data.data);
+          $scope.msg=data.data;
+          $scope.backBtnSuccess=false;
+        }else{
+          $scope.msg=data.data;
+          $scope.backBtnSuccess=true;
+        }
+      }
     });
   };
 });
@@ -13,7 +29,7 @@ app.factory('initService',function($http,$window,$httpParamSerializer){
   init.initial=function(password,conpassword,next){
     var data=$httpParamSerializer({password:password,conpassword:conpassword});
     $http.post("/init",data,{headers:{'Content-Type':'application/x-www-form-urlencoded'}}).then(function(data){
-        next(data);
+        next(data.data);
     },function(err){
         next(err);
     });
